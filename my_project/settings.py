aa29ec26fe5b27ebe1c715cebdaaa4434200f8e1
Mark Williams <maarkk@live.co.uk>
2025-04-23 16:47:44 +0100

    adding Heroku conif

--- my_project/settings.py
+++ my_project/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-ll%@gw@xqs=gh#xtjkm2ucm1_em(8nplt06dpic0d3h)_o6y$8'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
